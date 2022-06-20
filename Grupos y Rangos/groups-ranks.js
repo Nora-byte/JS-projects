b686fbfaecee88308aa621f557a3eefa5384d12c
DeveloperNora <yerko.fuentes@inacapmail.cl>
2022-06-19 23:29:25 -0400

    changes grupo y rangos

--- Grupos y Rangos/groups-ranks.js
+++ Grupos y Rangos/groups-ranks.js
@@ -3,8 +3,8 @@
 [] // Indican un intervalo de numero o caracteres
 
 [abc] // Uno de los caracteres a, b, c
-[^abc] // Cualquier caracter menos a, b, c
+// [^abc] --> Cualquier caracter menos a, b, c
 [a-z] // Cualquier minuscula entre a y z
 [0-9] // Cualquier numero entre 0 y 9
 [a-zA-Z] // Cualquier entre aA y zZ
-[a-zA-Z0-9-] // Cualquier letra, numero o guion
+// [a-zA-Z0-9-] --> Cualquier letra, numero o guion
