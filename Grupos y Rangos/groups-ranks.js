e4be9d64369ecff2dc1971d8350a1ed7cd052a02
DeveloperNora <yerko.fuentes@inacapmail.cl>
2022-06-19 23:26:40 -0400

    add Grupos y rangos

---
+++ Grupos y Rangos/groups-ranks.js
@@ -0,0 +1,10 @@
+/* Expresiones reguales, son aquellos intervalos que componen con numeros o caracteres */
+
+[] // Indican un intervalo de numero o caracteres
+
+[abc] // Uno de los caracteres a, b, c
+[^abc] // Cualquier caracter menos a, b, c
+[a-z] // Cualquier minuscula entre a y z
+[0-9] // Cualquier numero entre 0 y 9
+[a-zA-Z] // Cualquier entre aA y zZ
+[a-zA-Z0-9-] // Cualquier letra, numero o guion
