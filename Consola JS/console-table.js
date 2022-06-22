071438924426a198bcd289a9da8f078f68211ea7
DeveloperNora <yerko.fuentes@inacapmail.cl>
2022-06-21 23:27:24 -0400

    add consola JS

---
+++ Consola JS/console-table.js
@@ -0,0 +1,11 @@
+/* Console.table(), imprimir por consola un obj. o un array de manera grafica
+y sencilla de entender a simple vista*/
+
+const users =[
+    {name: 'Muriel', age: 22},
+    {name: 'Ariana', age: 23},
+    {name: 'Emma', age: 12},
+];
+
+// imprime en consola en forma de tabla
+console.table(users);
