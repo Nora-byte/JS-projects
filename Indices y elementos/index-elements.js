535f4e6282028ef1d35e4b58b9b1783b696132aa
DeveloperNora <yerko.fuentes@inacapmail.cl>
2022-06-21 20:14:23 -0400

    add indices y elementos

---
+++ Indices y elementos/index-elements.js
@@ -0,0 +1,12 @@
+/* Indide y elementos, en todo tipo de arrays que se compone de variables se puede
+revisar, recorrer y modificar el contenido de la misma arrays.*/
+
+const numbers = [1, 2, 3, 4, 5, 6, 7]
+
+// Se muestra el elemento que hay en el indice
+
+numbers.at(2) // 3
+numbers.at(-2) // 6
+
+numbers.at(0) // 1 (primer elemento)
+numbers.at(-1) // 7 (ultimo elemento)
