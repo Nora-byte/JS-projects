c825588f89d24d81052f2a66305e52c470dcc2b2
DeveloperNora <yerko.fuentes@inacapmail.cl>
2022-06-21 20:44:43 -0400

    add Array's

---
+++ Array's/N veces en JS/n-times-data.js
@@ -0,0 +1,7 @@
+/* Hacer algo N veces en JavaScript */
+
+Array.from({length: 5}).forEach(() =>{
+    console.log('do something')
+})
+
+// con ello, va a imprimir n veces el numero que se le ingresa a documento.
